51daa94d9b98038b4561b063699f84b70902a1dd
daniela0111 <95025799+daniela0111@users.noreply.github.com>
2023-05-17 11:45:53 +0200

    mobile version (navigation, countdown, logo)

---
+++ landingpage.js
@@ -0,0 +1,21 @@
+const targetDate = new Date("2023-08-25T18:00:00").getTime();
+
+function updateCountdown() {
+  const now = new Date().getTime();
+  const distance = targetDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+    "It's happening: " + days + " days  " + hours + "h " + minutes + "m " + seconds + "s";
+
+  if (distance < 0) {
+    clearInterval(countdownInterval);
+    document.getElementById("countdown").innerHTML = "today";
+  }
+}
+
+const countdownInterval = setInterval(updateCountdown, 1000);
